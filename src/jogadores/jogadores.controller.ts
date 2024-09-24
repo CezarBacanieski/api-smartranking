65ea162fb0ffc1a36255d82476b8de8c42fdad26
Cezar_Bacanieski <bacanieskidev@gmail.com>
2024-09-24 19:41:49 -0300

    feat: creating a custom pipe

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -11,6 +11,7 @@ import {
 import { CriarJogadorDto } from './dtos/create-jogador.dto';
 import { JogadoresService } from './jogadores.service';
 import { Jogador } from './interfaces/jogador.interface';
+import { JogadoresValidacaoParametrosPipe } from './pipes/jogadores-validation-params.pipe';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -23,7 +24,7 @@ export class JogadoresController {
 
   @Get()
   async consultarJogadores(
-    @Query('email') email: string,
+    @Query('email', JogadoresValidacaoParametrosPipe) email: string,
   ): Promise<Jogador[] | Jogador> {
     if (email) {
       return await this.jogadoresService.consultarJogadoresPeloEmail(email);
@@ -33,7 +34,9 @@ export class JogadoresController {
   }
 
   @Delete()
-  async deletarJogardor(@Query('email') email: string): Promise<void> {
+  async deletarJogardor(
+    @Query('email', JogadoresValidacaoParametrosPipe) email: string,
+  ): Promise<void> {
     await this.jogadoresService.deletarJogador(email);
   }
 }
